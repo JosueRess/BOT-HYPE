835d9ba2e36941c610699596176e3e07dce3cbf4
JosueRess <sousajosue177@gmail.com>
2026-03-11 16:10:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
 });
 
 const fila = [];
